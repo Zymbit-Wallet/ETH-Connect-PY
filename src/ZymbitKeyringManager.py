a8767d87a19132fe3e13577c2e14d10b07e3f18b
Shivaansh Kapoor <shivaanshkapoor02@gmail.com>
2023-04-19 17:07:53 -0700

    error checking and type hinting

--- src/ZymbitKeyringManager.py
+++ src/ZymbitKeyringManager.py
@@ -15,23 +15,25 @@ class ZymbitKeyringManager():
         if (not issubclass(keyringClass, Keyring)):
             raise TypeError(f"Invalid type: {type(keyringClass)}. Expected a subclass of the Keyring class")
 
-        if(len(walletName) < 1):
-             raise TypeError("Invalid walletName")
+        if (len(walletName) < 1 or not isinstance(walletName, str)):
+             raise ValueError("Invalid walletName")
         
-        if(not isinstance(masterGenKey, bytearray)):
+        if (not isinstance(masterGenKey, bytearray)):
             raise TypeError("Invalid masterGenKey")
+        
+        try:
+            use_BIP39_recovery = zymkey.RecoveryStrategyBIP39()
+            keyType: str = keyringClass.CURVE.getCurveType()
+            masterKey: tuple[int, str] = zymkey.client.gen_wallet_master_seed(key_type = keyType, master_gen_key = masterGenKey, wallet_name = walletName, recovery_strategy = use_BIP39_recovery)
 
-        use_BIP39_recovery = zymkey.RecoveryStrategyBIP39()
-        keyType = keyringClass.CURVE.getCurveType()
-        masterKey = zymkey.client.gen_wallet_master_seed(key_type = keyType, master_gen_key = masterGenKey, wallet_name = walletName, recovery_strategy = use_BIP39_recovery)
-
-        options = {
-            "walletName": walletName,
-        }
-        keyring: keyringClass = keyringClass(options)
-        self.keyrings.append(keyring)
-
-        return masterKey
+            options = {
+                "walletName": walletName
+            }
+            keyring: keyringClass = keyringClass(options)
+            self.keyrings.append(keyring)
+            return masterKey
+        except:
+            raise ValueError("Failed to create keyring")
 
 
     def addKeyring(self, keyring: Keyring):
